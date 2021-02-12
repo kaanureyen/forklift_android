204de364d4b5f4d2b330c23d3a1a90ba95e2ea99
Kaan Üreyen <kaanureyen07@gmail.com>
2021-02-12 23:56:01 +0300

    v2: bt connection is working ^^ todo: add left right switch add inversion toggle individually for l, r, and stepper

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:control_pad/control_pad.dart';
 import 'package:flutter/services.dart';
-import 'package:toggle_switch/toggle_switch.dart';
 import 'dart:math';
-//import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,]);
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return MaterialApp(
       title: 'Forklift Remote',
       theme: ThemeData(
@@ -33,16 +35,72 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
+  BluetoothConnection connection;
+  String t = "";
+  int l = 0;
+  int r = 0;
+  String toSend = "";
+  int sn = 0;
+  String st = "";
 
-  void _incrementCounter() {
-    setState(() {
-    //  _counter++;
-    });
+  void sending() async {
+    String c1 = "";
+    String c2 = "";
+    String m1 = "";
+    String m2 = "";
+    if(l > 0){
+      c1 = "C,1,1";
+    }else{
+      c1 = "C,1,2";
+    }
+    if(r>0){
+      c2 = "C,2,1";
+    }
+    else{
+      c2 = "C,2,2";
+    }
+    m1 = "M,1," + l.toInt().abs().toString();
+    m2 = "M,2," + r.toInt().abs().toString();
+    toSend = c1+c2+m1+m2+st;
+    st = "";
+    connection.output
+        .add(Uint8List.fromList(ascii.encode(toSend)));
+    await Future.delayed(Duration(milliseconds: 66));
+    sending();
+  }
+
+  void bt() async {
+    try {
+      connection = await BluetoothConnection.toAddress("00:19:10:08:D5:05");
+      setState(() {
+        t = "Connected to the device";
+      });
+
+      print(t);
+      if(sn == 0){
+        sending();
+        sn = 1;
+      }
+
+      connection.input.listen((Uint8List data) {
+        print('Data incoming: ${ascii.decode(data)}');
+      }).onDone(() {
+        setState(() {
+          t = 'Disconnected by remote request';
+        });
+
+        print(t);
+      });
+    } catch (exception) {
+      setState(() {
+        t = 'Cannot connect, exception occured: ' + exception;
+      });
+
+      print(t);
+    }
   }
 
   @override
@@ -52,34 +110,53 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            JoystickView(
-              onDirectionChanged: (deg,val){
-                double hor = (sin(deg/360*(2*pi))*val);
-                double shor = hor.sign;
-                double ver = (cos(deg/360*(2*pi))*val);
-                double sver = ver.sign;
-                int left = (((sver)*pow(ver, 2) + (shor)*pow(hor, 2))*255).round();
-                int right = (((sver)*pow(ver, 2) - (shor)*pow(hor, 2))*255).round();
-                print('left $left right $right');},
-            ),
-            ToggleSwitch(
-              initialLabelIndex: 1,
-              labels: ['Up','Stop','Down'],
-              icons: [Icons.arrow_upward, Icons.stop, Icons.arrow_downward],
-              onToggle: (index) {
-                print('switched to: $index');
-              },
-            ),
-          ],
-        ),
+        child: Column(children: <Widget>[
+          Text(t),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              JoystickView(
+                onDirectionChanged: (deg, val) {
+                  double hor = (sin(deg / 360 * (2 * pi)) * val);
+                  double shor = hor.sign;
+                  double ver = (cos(deg / 360 * (2 * pi)) * val);
+                  double sver = ver.sign;
+                  int left =
+                      (((sver) * pow(ver, 2) + (shor) * pow(hor, 2)) * 255)
+                          .round();
+                  int right =
+                      (((sver) * pow(ver, 2) - (shor) * pow(hor, 2)) * 255)
+                          .round();
+                  //print('left $left right $right');
+                  l = left;
+                  r = right;
+                },
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_upward),
+                    onPressed: () {
+                      st = "S";
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_downward),
+                    onPressed: () {
+                      st = "s";
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: bt,
+        tooltip: 'Connect',
+        child: Icon(Icons.bluetooth_connected),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
